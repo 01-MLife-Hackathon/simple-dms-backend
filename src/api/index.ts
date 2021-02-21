73b480fa21332ec97834eaab2ec9283b8a8c4bc7
titanv <a01098930999@gmail.com>
2021-02-21 23:24:04 +0900

    update - route

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,16 @@ import Router from '@koa/router';
 
 const api = new Router();
 
-import {login, extension, homecoming, reissuance, extensionInfo, extensionCancel} from './api.controller';
+import {login, extension, homecoming, homecomingInfo, reissuance, extensionInfo, extensionCancel, meal} from './api.controller';
 
 api.post('/auth', login);
 api.post('/extension', extension);
 api.post('/homecoming', homecoming);
+api.post('/homecomingInfo', homecoming);
 api.get('/reissuance', reissuance);
 api.get('/extensionInfo', extensionInfo);
 api.delete('/extensioncancel', extensionCancel);
+api.get('/meal', meal);
 
 
 export default api;
